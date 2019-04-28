88a7951e766e5c5a8d6a087824beb5b01853d9da
Sk-Riyaz <skriyaz09@gmail.com>
2019-04-29 01:24:26 +0530

    Updated createUser to avoid manual creation of default user

--- createUser.py
+++ createUser.py
@@ -1,7 +1,14 @@
 from app.models import Roles, User
-from app import db
+from app import db, create_app
+from config import Config
 
+"""
 def create_super():
+    app = create_app(Config)
+    app_context = app.app_context()
+    app_context.push()
+    db.create_all()
+
     #role = Roles.query.filter_by(name="").first()
     try:
         role_s = Roles(name="SUPER_USER")
@@ -14,7 +21,7 @@ def create_super():
     except Exception as e:
         db.session.rollback()
         print(e)
-    
+
     try:
         role_id = Roles.query.filter_by(name="SUPER_USER").first()
         user = User(username="super",
@@ -26,4 +33,50 @@ def create_super():
     except Exception as e:
         print(e)
         db.session.rollback()
-create_super()
+"""
+
+def create_roles():
+    app = create_app(Config)
+    app_context = app.app_context()
+    app_context.push()
+    db.create_all()
+    if Roles.query.all():
+        return True
+    try:
+        role_s = Roles(name="SUPER_USER")
+        role_a = Roles(name="ADMINISTRATOR")
+        role_u = Roles(name="USER")
+        db.session.add(role_s)
+        db.session.add(role_a)
+        db.session.add(role_u)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+        return False
+    return True
+
+
+def create_super_user():
+    app = create_app(Config)
+    app_context = app.app_context()
+    app_context.push()
+    db.create_all()
+    if User.query.all():
+        return True
+    try:
+        role_id = Roles.query.filter_by(name="SUPER_USER").first()
+        user = User(username="super", email="super@mymail.com", role_id=role_id.id)
+        user.set_password("super")
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+        return False
+    return True
+
+
+#create_super()
+create_roles()
+create_super_user()
--- ricemill.py
+++ ricemill.py
@@ -4,10 +4,10 @@ from app.models import User, Roles
 app = create_app()
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Roles': Roles}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
