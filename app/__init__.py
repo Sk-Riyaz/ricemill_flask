b65f4f56e648cae56049f1c960ccedc54ce0b575
Riyaz Shaik <shakry@amazon.com>
2019-05-06 16:11:49 +0530

    flask_moment for localtime display

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
 from config import Config
 #from flask_user import UserManager
 #from app.models import User
@@ -13,6 +14,7 @@ from config import Config
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
+moment = Moment()
 login.login_view = 'auth.login'
 login.login_message = 'Please log in to access this page.'
 
@@ -29,7 +31,7 @@ def create_app(config_class=Config):
     login.init_app(app)
     #mail.init_app(app)
     #bootstrap.init_app(app)
-    #moment.init_app(app)
+    moment.init_app(app)
     #babel.init_app(app)
 
     from app.errors import bp as errors_bp
