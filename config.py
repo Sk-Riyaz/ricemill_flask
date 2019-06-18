58b55e75a10ac245fa3bf1d92593f614dcff38f2
Sk-Riyaz <skriyaz09@gmail.com>
2019-06-18 07:22:09 +0530

    Added Purchase object to retrive from terminal

--- config.py
+++ config.py
@@ -5,10 +5,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    #set SQLALCHEMY_DATABASE_URI =myql://shaik:toor@localhost/ricemill
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SUPER_USER_STR = "SUPER_USER"
--- ricemill.py
+++ ricemill.py
@@ -1,12 +1,12 @@
 from app import create_app, db
-from app.models import User, Roles
+from app.models import User, Roles, Purchase, Sale
 
 app = create_app()
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Roles': Roles}
+    return {'db': db, 'User': User, 'Roles': Roles, 'Purchase': Purchase}
 
 
 if __name__ == "__main__":
