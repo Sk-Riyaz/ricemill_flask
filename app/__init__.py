57dd5b1af3ba43fa4f9baa4abd3f6fb277547913
Sk-Riyaz <skriyaz09@gmail.com>
2019-07-04 01:17:59 +0530

    Adding all nav bar links

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,8 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_moment import Moment
 from config import Config
-#from flask_user import UserManager
-#from app.models import User
+# from flask_user import UserManager
+# from app.models import User
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -29,10 +29,10 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
-    #mail.init_app(app)
-    #bootstrap.init_app(app)
+    # mail.init_app(app)
+    # bootstrap.init_app(app)
     moment.init_app(app)
-    #babel.init_app(app)
+    # babel.init_app(app)
 
     from app.errors import errors_bp
     app.register_blueprint(errors_bp)
